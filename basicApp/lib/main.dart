b0534e342d9840fec03b96c4ab6a20fe9c940481
kiraci <osmanbugraaydin@gmail.com>
2020-09-09 15:37:44 +0300

    World Time App

--- basicApp/lib/main.dart
+++ basicApp/lib/main.dart
@@ -1,54 +1,6 @@
 import 'package:flutter/material.dart';
-import 'names.dart';
-import 'namecard.dart';
+import 'pages/home.dart';
 
 void main() => runApp(MaterialApp(
-  home: QuoteList(),
+  home: Home(),
 ));
-
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  List<Names> names = [
-    Names( property: "My name", name: "Osman"),
-    Names( property: "My name", name: "Buğra"),
-    Names( property: "My name", name: "Aydın"),
-    Names( property: "My friend", name: "Çagatay"),
-    Names( property: "My friend", name: "Kadir"),
-    Names( property: "My friend", name: "Fatih"),
-    Names( property: "My friend", name: "Osman"),
-    Names( property: "My friend", name: "Doğan"),
-    Names( property: "My friend", name: "Yahya"),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[700],
-      appBar: AppBar(
-        title: Text("Some Turkish Names"),
-        centerTitle: true,
-        backgroundColor: Colors.red[300],
-      ),
-      body: SingleChildScrollView(
-              child: Center(
-          child: Column(
-            children: names.map(( name ) => NameCard(
-              names: name,
-              delete: () {
-                setState(() {
-                  names.remove(name);
-                });
-              },
-              )).toList(),
-          ),
-        ),
-      ),
-    );
-  }
-}
--- basicApp/lib/names.dart
+++
@@ -1,8 +0,0 @@
-class Names {
-
-  String property;
-  String name;
-
-  Names({ this.property, this.name });
- 
-}
