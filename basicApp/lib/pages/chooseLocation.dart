559e635aed890990a6629b683c25150df306df83
kiraci <osmanbugraaydin@gmail.com>
2020-09-09 16:42:01 +0300

    Asynchronous Code has been learned

--- basicApp/lib/pages/chooseLocation.dart
+++ basicApp/lib/pages/chooseLocation.dart
@@ -7,6 +7,34 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() async {
+
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return "osman";
+    });
+
+    String password = await Future.delayed( Duration(seconds: 3), () {
+      return "osman123.";
+    });
+
+    if( username == "osman"  && password == "osman123."){
+      print("user logged in...");
+    }else{
+      print("user could not log in...");
+    }
+
+    print( '$username - $password' );
+
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
